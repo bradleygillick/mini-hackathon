a91d28da99a9340f6cf23100c98d719028e387b9
Jules Perryman <jules.perryman@gmail.com>
2017-03-03 14:31:44 -0700

    updated files

--- mini_hackathon.js
+++ mini_hackathon.js
@@ -1,47 +1,25 @@
-
 $(document).ready(function() {
 
+            $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/todayilearned/top/.json', function(data) {
 
-$.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/todayilearned/top/.json', function(data){
-    var thumbnail = data.data.children[1].data.thumbnail
-    console.log(thumbnail);
-    var title = data.data.children[1].data.title
-    console.log(title);
-    $('.redditResult').append("<img src='" + thumbnail + "'>")
-    $('.title1').append("<h4> Title: </h4>" + title)
-    // $('.link')
+                for (var i = 0; i < 11; i++) {
+                    var thumbnail = data.data.children[i].data.thumbnail
+                    console.log(thumbnail);
+                    var title = data.data.children[i].data.title
+                    console.log(title);
 
-})
-// var reddit = function() {
-//     var url = "https://www.reddit.com/r/todayilearned/.json"
-//     // url += $("input").val()
-//     console.log(url);
 
-    // $.get(url, function(data) {
-    //     console.log(data);
-    //     var title = data.Title
-    //     console.log(title, poster);
-    //     var poster = data.Poster
-    //     updatePage(title, poster)
-    // })
-    // // .then(function(){
-    // //     showMessage()
-    // // })
-    //
-    //
-    // function updatePage(title,poster) {
-    //     $(".title").append(title)
-    //     console.log(poster);
-    //     $(".poster").append('<img src="" '> + poster)
-    // }
+                    $('.redditResult').append("<img src='" + thumbnail + "'>", title, "<br>")
+                    // $('.title1').append("<h4> Title: </h4>" + title)
+                }
+
+            })
+
 });
 
 
-
-
-
 // Pokemon Ajax call Test API
-
+//
 // $.ajax({
 //         url: 'http://pokeapi.co/api/v2/pokemon/25',
 //         type: 'GET',
@@ -61,5 +39,3 @@ $.get('https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/todayilearn
 //                "<img src='" + results.sprites.front_shiny + "'>");
 //             }
 //     })
-
-// });
